d40d49e39763380261a48551780a924c1d66a591
sedlacekradek <113298261+sedlacekradek@users.noreply.github.com>
2022-11-30 23:13:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from web import create_app
 
 
-# app = create_app()
+app = create_app()
 
 
 ### TO RUN LOCALLY ###
-if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app = create_app()
+#     app.run(debug=True)
